f7c4087b21a042bb1aec4f19997e2cb969f5a9a4
Denis Kolosov <ftforest640@gmail.com>
2023-02-02 16:20:05 +0700

    create project front end bible

---
+++ gulp/tasks/html.js
@@ -0,0 +1,44 @@
+import webpHtml from 'gulp-webp-html-nosvg'
+import versionNumber from 'gulp-version-number'
+import htmlMin from 'gulp-htmlmin'
+import pug from 'gulp-pug'
+
+export const html = () => {
+  return app.gulp.src(app.path.src.html)
+    .pipe(app.plugins.plumber(
+      app.plugins.notify.onError({
+        title: 'HTML',
+        message: 'Error: <%= error.message %>'
+      })
+    ))
+    .pipe(pug({
+      // Сжатие HTML файфайла
+      pretty: true,
+      // Показывать в терминале какой файл обработан
+      verbose: true
+    }))
+    .pipe(app.plugins.replace(/@img\//g, 'images/'))
+    .pipe(app.plugins.if(app.isBuild, webpHtml()))
+    .pipe(htmlMin({
+      useShortDoctype: true,
+      sortClassName: true,
+      collapseWhitespace: app.isBuild,
+      removeComments: app.isBuild
+    }))
+    .pipe(app.plugins.if(app.isBuild, versionNumber({
+      'value': '%DT%',
+      'append': {
+        'key': '_v',
+        'cover': 0,
+        'to': [
+          'css',
+          'js',
+        ],
+      },
+      'output': {
+        'file': 'gulp/version.json'
+      },
+    })))
+    .pipe(app.gulp.dest(app.path.build.html))
+    .pipe(app.plugins.browserSync.stream())
+}
